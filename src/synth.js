cb2d5f234f396ba838681d92f77d4980108cd88b
mosm <airman515@gmail.com>
2020-11-21 14:37:41 +0900

    Fix sample rate

--- src/synth.js
+++ src/synth.js
@@ -2,7 +2,18 @@ import * as Tone from 'tone';
 import worklet from '-!url-loader?limit=false!./worklet';
 
 export class Synth {
-    constructor() {
+    async setup() {
+        const context = new AudioContext({ sampleRate: 44100 });
+        Tone.setContext(new Tone.Context(context));
+
+        await context.audioWorklet.addModule(worklet, { credentials: 'omit' });
+        const workletNode = new AudioWorkletNode(context, 'main', {
+            numberOfInputs: 0,
+            numberOfOutputs: 1,
+            outputChannelCount: [1],
+        });
+        this._port = workletNode.port;
+
         this._input = new Tone.Gain();
         this._chorus = new Tone.Chorus(2, 2, 0.1).start();
         this._reverb = new Tone.Reverb().set({
@@ -13,27 +24,15 @@ export class Synth {
         this._gain = new Tone.Gain(1);
         const limiter = new Tone.Limiter(-20);
 
-        this._input.chain(
+        Tone.connectSeries(
+            workletNode,
+            this._input,
             this._chorus,
             this._reverb,
             this._gain,
             limiter,
-            Tone.Destination
+            context.destination
         );
-    }
-
-    async setup() {
-        const context = Tone.getContext();
-
-        await context.addAudioWorkletModule(worklet, 'main');
-        const workletNode = context.createAudioWorkletNode('main', {
-            numberOfInputs: 0,
-            numberOfOutputs: 1,
-            outputChannelCount: [1],
-        });
-        this._port = workletNode.port;
-
-        Tone.connect(workletNode, this._input);
     }
 
     set volume(value) {
